2f41aa10bdb145c0ad1b4f302986f6af6bfd946f
arutan <111817936+arutan400@users.noreply.github.com>
2023-05-31 17:37:30 +0900

    Update Lite_1.py

--- Lite_1.py
+++ Lite_1.py
@@ -1,1 +1,13 @@
+import onnxruntime as rt
+import numpy as np  # この行を追加
 
+# モデルをロードします。
+sess = rt.InferenceSession("efficientnet_lite0.onnx")
+
+# モデルを使用して予測を行います。
+# ここではダミーデータを使用しますが、実際には入力データを指定します。
+dummy_input = np.random.randn(1, 224, 224, 3).astype(np.float32)
+input_name = sess.get_inputs()[0].name
+result = sess.run(None, {input_name: dummy_input})
+
+print(result)
